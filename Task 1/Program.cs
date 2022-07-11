776b5461eb9882946bf48e4c8e0eed69b0dc9b5a
finall2022 <finall.git2022@gmail.com>
2022-07-11 12:27:25 +0300

    дз к 9-му семинару

---
+++ Task 1/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 1: Задайте значения M и N. Напишите программу, которая
+выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+M = 1; N = 5 -> "2, 4"
+M = 4; N = 8 -> "4, 6, 8" */
+
+int Even(int mTemp)
+{
+    if (mTemp % 2 == 0) return mTemp;
+    return Even(mTemp + 1);
+}
+
+Console.WriteLine("Программа нахождения четных чисел в промежутке от M до N");
+Console.Write("Введите M: ");
+string sm = Console.ReadLine();
+int m = int.Parse(sm);
+Console.Write("Введите N: ");
+string sn = Console.ReadLine();
+int n = int.Parse(sn);
+
+while (m <= n)
+{
+    Console.Write($"{Even(m)}\t");
+    m = m + 2;
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 2: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8 -> 30 */
+
+int Sum(int mTemp, int nTemp, int sTemp)
+{
+
+    if (mTemp > nTemp) return sTemp;
+    return Sum(mTemp + 1, nTemp, sTemp = sTemp + mTemp);
+}
+
+Console.WriteLine("Программа нахождения суммы натуральных чисел в промежутке от M до N");
+Console.Write("Введите M: ");
+string sm = Console.ReadLine();
+int m = int.Parse(sm);
+Console.Write("Введите N: ");
+string sn = Console.ReadLine();
+int n = int.Parse(sn);
+
+/* int sum = 0;
+while (m <= n)
+{
+    sum = sum + m;
+    m++;
+} */
+int sum = 0;
+Console.Write(Sum(m, n, sum));
